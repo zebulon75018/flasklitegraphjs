c4a5d2db3f91540b1aa339abdb047754d272b0bc
zebulon75018 <zebulon75018@yahoo.fr>
2020-08-29 00:25:08 +0200

    Update app.py
    seem not usefull the import at the begining

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from scripts import tabledef
-from scripts import forms
-from scripts import helpers
+#from scripts import tabledef
+#from scripts import forms
+#from scripts import helpers
 from flask import Flask, redirect, url_for, render_template, request, session
 from flask import jsonify
 
