ad05f83fb8ae671f2365fa9f42dd003d462c8778
zebulon75018 <zebulon75018@yahoo.fr>
2020-11-25 22:30:02 +0100

    change with fastapi and baklavajs

--- app.py
+++ app.py
@@ -1,88 +1,22 @@
-# -*- coding: utf-8 -*-
-
-#from scripts import tabledef
-#from scripts import forms
-#from scripts import helpers
-from flask import Flask, redirect, url_for, render_template, request, session
-from flask import jsonify
-
-import json
-import sys
-import os
-from os import listdir
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import uvicorn
 
 
-app = Flask(__name__)
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
-# ======== Routing =========================================================== #
-# -------- Login ------------------------------------------------------------- #
-@app.route('/', methods=['GET', 'POST'])
-def login():
-
-	return render_template('index.html', files = listdir("data"))
-
-@app.route("/load/", methods=['POST'])
-def load():
-	print(str(request.form['name']))
-	with open(os.path.join("data",request.form['name'])) as file:
-		d = json.load(file)
-		print(d)
-		print(type(d))
-		return jsonify(d)
-	
-@app.route("/save/", methods=['POST'])
-def save():
-   #print(request.blueprint)
-   with open(os.path.join("data",request.form['name']+".txt"),"w") as file:  
-	file.write(request.form['editor'])
-	print(request.form['editor'])
-   return ""
-   
-@app.route("/logout")
-def logout():
-    session['logged_in'] = False
-    return redirect(url_for('login'))
+templates = Jinja2Templates(directory="templates")
 
 
-# -------- Signup ---------------------------------------------------------- #
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    if not session.get('logged_in'):
-        form = forms.LoginForm(request.form)
-        if request.method == 'POST':
-            username = request.form['username'].lower()
-            password = helpers.hash_password(request.form['password'])
-            email = request.form['email']
-            if form.validate():
-                if not helpers.username_taken(username):
-                    helpers.add_user(username, password, email)
-                    session['logged_in'] = True
-                    session['username'] = username
-                    return json.dumps({'status': 'Signup successful'})
-                return json.dumps({'status': 'Username taken'})
-            return json.dumps({'status': 'User/Pass required'})
-        return render_template('login.html', form=form)
-    return redirect(url_for('login'))
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "id": id})
 
-
-# -------- Settings ---------------------------------------------------------- #
-@app.route('/settings', methods=['GET', 'POST'])
-def settings():
-    if session.get('logged_in'):
-        if request.method == 'POST':
-            password = request.form['password']
-            if password != "":
-                password = helpers.hash_password(password)
-            email = request.form['email']
-            helpers.change_user(password=password, email=email)
-            return json.dumps({'status': 'Saved'})
-        user = helpers.get_user()
-        return render_template('settings.html', user=user)
-    return redirect(url_for('login'))
-
-
-# ======== Main ============================================================== #
 if __name__ == "__main__":
-    app.secret_key = os.urandom(12)  # Generic key for dev purposes only
-    app.run(debug=True, use_reloader=True)
+    uvicorn.run("app:app", host="127.0.0.1", port=5000, debug=True,log_level="info")
+
